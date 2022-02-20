b4a68a92be342d14dc7104bcd0a0f1d0c2c2b1e3
JakeTSherman <jake.sherman@kellogg.northwestern.edu>
2022-02-20 03:19:14 +0000

    pause

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,8 @@ class PostsController < ApplicationController
     
       def new
         @post = Post.new
-        @post.place_id = params["place_id"]
+        @place = Place.find(params["places_id"])
+        @post.place_id = @place.id
       end
     
       def create
